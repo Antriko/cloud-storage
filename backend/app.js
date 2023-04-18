be337fc30d205228cdfe444c3d806778f8048003
Antriko <creeperdrag772@gmail.com>
2023-04-18 22:17:25 +0100

    init

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to database
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+connectionString = `mongodb://${process.env.DB_SERVICE_NAME}:27017/${process.env.DB_DATABASE}`
+mongoose.connect(connectionString, {
+    authSource: "admin",
+    user: process.env.DB_USERNAME,
+    pass: process.env.DB_PASSWORD
+})
+.then(() => console.log("Connected to database"))
+.catch(e => console.log("ERROR?", e))
+
+// parse and session
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+app.use(session({
+    secret: 'foo',
+    store: MongoStore.create({
+        mongoUrl: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVICE_NAME}:27017/`, 
+        dbName: process.env.DB_DATABASE
+    }),
+    resave: false,
+    saveUninitialized: true
+}));
+
+// user rest api
+var user = require('./user.js');
+app.use('/api/user', user)
+
+// storage rest api
+var storage = require('./storage.js');
+app.use('/api/storage', storage)
+
+
+const PORT = 3010;
+app.listen(PORT);
+console.log("Backend started")
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main>
+        <div className="relative px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+                <div>
+                    <div>
+                        <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                            Cloud Storage
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-900 sm:text-center">
+                            Retain your data online securely yourself.
+                        </p>
+                        <div className="mt-8 flex gap-x-4 sm:justify-center">
+                            {}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
